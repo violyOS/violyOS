bc0576cd6c5d1f7f3a1a2796775e85419706b64b
lukezielke <neongelb99@gmail.com>
2020-02-21 13:02:16 +0100

    changed everything so not describeable :)

--- source/airootfs/root/customize_airootfs.sh
+++ source/airootfs/root/customize_airootfs.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
+
 set -e -u
 
-# Locale
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "LC_COLLATE=C" >> /etc/locale.conf
 
-# virtual console
-echo "KEYMAP=us" > /etc/vconsole.conf
-
-# Time and clock
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-hwclock --systohc --utc
 
-# virtual console
-echo "KEYMAP=us" > /etc/vconsole.conf
-
-# hostname
-echo "violyOS" > /etc/hostname
-
-# root and live user
-usermod -s /bin/bash root
+usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash liveuser
-#chmod 750 /etc/sudoers.d
-chmod 440 /etc/sudoers.d/g_wheel
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh liveuser
+#chmod 700 /root
 chown -R liveuser:users /home/liveuser
 
-# Uncomment mirrors and amend journald
+
+
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
-# Enable services
-systemctl enable choose-mirror.service
-systemctl set-default multi-user.target
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
+systemctl enable pacman-init.service choose-mirror.service
+systemctl set-default multi-user.target
+systemctl enable NetworkManager
+rm /etc/udev/rules.d/81-dhcpcd.rules
+pacman -Syy
